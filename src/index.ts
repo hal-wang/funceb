be2da1b37a5c183913b5140c8b08e30806f04f14
hal-wang <hi@hal.wang>
2021-05-10 13:38:30 +0800

    feat: trim basePath

--- src/index.ts
+++ src/index.ts
@@ -5,28 +5,24 @@ import { getType } from "mime";
 export default class Funcweb {
   private readonly filePath: string;
 
-  constructor(url: string, basePath?: string) {
-    if (url.endsWith("/")) {
-      url = url.substr(0, url.length - 1);
-    }
-    if (url.startsWith("/")) {
-      url = url.substr(1, url.length - 1);
-    }
-    if (basePath && !url.startsWith(basePath)) {
-      url = `${basePath}/${url}`;
+  constructor(reqPath: string, webPath?: string) {
+    reqPath = this.trimPath(reqPath);
+    if (webPath) {
+      webPath = this.trimPath(webPath);
+      reqPath = `${webPath}/${reqPath}`;
     }
 
-    this.filePath = path.join(process.cwd(), url);
+    this.filePath = path.join(process.cwd(), reqPath);
     if (fs.existsSync(this.filePath) && fs.lstatSync(this.filePath).isFile()) {
       return;
     }
 
-    this.filePath = path.join(process.cwd(), url, "index.html");
+    this.filePath = path.join(process.cwd(), reqPath, "index.html");
     if (fs.existsSync(this.filePath) && fs.lstatSync(this.filePath).isFile()) {
       return;
     }
 
-    this.filePath = path.join(process.cwd(), basePath || "", "404.html");
+    this.filePath = path.join(process.cwd(), webPath || "", "404.html");
     if (fs.existsSync(this.filePath) && fs.lstatSync(this.filePath).isFile()) {
       return;
     }
@@ -52,5 +48,15 @@ export default class Funcweb {
     }
 
     return getType(this.filePath) || "*/*";
+  }
+
+  private trimPath(path: string): string {
+    if (path.endsWith("/")) {
+      path = path.substr(0, path.length - 1);
+    }
+    if (path.startsWith("/")) {
+      path = path.substr(1, path.length - 1);
+    }
+    return path;
   }
 }
